0016a08208ee304ea578953eee1e2fa7cc1b98ca
TeaMaster1337 <vovachernyh3@gmail.com>
2026-03-20 16:44:27 +0200

    New Changes

--- new.js
+++ new.js
@@ -1,9 +1,17 @@
 const departments = {
   qa: ["Anna", "Bob"],
-  dev: ["John", ["Kate"], "Igor"],
+  dev: ["John", "Kate", "Igor"],
   hr: ["Liza"],
 };
 function getAllEmployees(departments) {
-  return Object.values(departments).flat(Infinity);
+  const employees = [];
+  for (const key in departments) {
+    if (departments.hasOwnProperty(key)) {
+      employees.push(...departments[key].flat(Infinity));
+    }
+  }
+  return employees;
 }
 console.log(getAllEmployees(departments));
+
+// Comment
